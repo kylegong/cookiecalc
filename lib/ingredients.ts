c77855917167339d146020143d1f4d8722cbac77
KG <kylegong+git@gmail.com>
2021-01-03 10:14:05 -0800

    Update ingredients

--- lib/ingredients.ts
+++ lib/ingredients.ts
@@ -4,8 +4,24 @@ export interface Ingredient {
 }
 
 export const INGREDIENTS: Ingredient[] = [
-  { name: "brown sugar", cup_weight: 200 },
-  { name: "sugar", cup_weight: 200 },
-  { name: "flour", cup_weight: 120 },
+  { name: "baking powder", cup_weight: 235 },
+  { name: "baking soda", cup_weight: 220 },
+  { name: "brown sugar, packed", cup_weight: 198 },
+  { name: "butter", cup_weight: 227 },
+  { name: "cocoa powder", cup_weight: 85 },
+  { name: "cornmeal", cup_weight: 148 },
+  { name: "cornstarch", cup_weight: 133 },
+  { name: "flour, all-purpose", cup_weight: 142 },
+  { name: "flour, bread", cup_weight: 163 },
+  { name: "flour, cake", cup_weight: 113 },
+  { name: "flour, whole wheat", cup_weight: 156 },
   { name: "milk", cup_weight: 245 },
+  { name: "salt, kosher (Diamond Crystal)", cup_weight: 140 },
+  { name: "salt, kosher (Morton's)", cup_weight: 225 },
+  { name: "salt, table", cup_weight: 280 },
+  { name: "sugar, confectioner's/powdered", cup_weight: 113 },
+  { name: "sugar, granulated/white", cup_weight: 198 },
+  { name: "vanilla extract", cup_weight: 208 },
+  { name: "water", cup_weight: 237 },
+  { name: "yeast, active dry", cup_weight: 136 },
 ];
